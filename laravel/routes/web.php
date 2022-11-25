e5decf63a53414683a49baf53ed3fa4497dc4282
Joel donaire diaz <jododi@fp.insjoaquimmir.cat>
2022-11-25 18:36:58 +0100

    M09

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Http\Controllers\MailController;
 use App\Http\Controllers\FileController;
