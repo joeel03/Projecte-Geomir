b41cbb202ee39575e44574db23e8c4ecdcd146d4
xagape1 <xagape@fp.insjoaquimmir.cat>
2023-01-24 19:43:41 +0100

    CONTACTE MAPA INDIVIDAUL

--- laravel/resources/views/contacte.blade.php
+++ laravel/resources/views/contacte.blade.php
@@ -1,15 +1,35 @@
 @extends('layouts.box-app')
 @section('box-content')
+<head>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+@import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css");
+</head>
 <header>
-<section class="showcase">
+	<section class="showcase">
 			<div class="video-container">
 				<video src="https://traversymedia.com/downloads/video.mov" autoplay muted loop></video>
 			</div>
 			<div class="content">
-				<h1>Shoot For The Stars</h1>
-				<h3>Full screen video landing page</h3>
-				<a href="#about" class="btn">Read More</a>
+				<h1 class="h1-contacte-h">Contacta'ns!</h1>
+				<h3 class="h3-contacte-h">Envia el teu missatge</h3>
+				<a class="boto-contacte" href="#about" >Read More</a>
+			</div>
+	</section>
+</header>
+<body class="body-contacte">
+	<h1 class="h1-contacte-b">Vols visitar-nos?</h1>
+	<h3 class="h3-contacte-b">Ubica'ns al mapa</h3>
+	<img class="img-contacte" src="img/2023-01-24_17-12.png"></img>
+</body>
+<footer>
+<section id="about">
+			<h2>Segueix-nos a xarxes!</h2>
+			<div class="social">
+				<a href="https://github.com/bradtraversy" target="_blank"><i class="bi bi-twitter"></i></a>
+				<a href="https://facebook.com/traversymedia" target="_blank"><i class="bi bi-facebook"></i></a>
+				<a href="https://twitter.com/traversymedia" target="_blank"><i class="bi bi-linkedin"></i></a>
+				<a href="https://www.linkedin.com/in/bradtraversy" target="_blank"><i class="bi bi-github"></i></a>
 			</div>
 		</section>
-    </header>
+</footer>
 @endsection
