7b2a015006f62238a897e5a75681a0aab880a8c1
xagape1 <xagape@fp.insjoaquimmir.cat>
2023-02-06 16:30:01 +0100

    Contacte falta keyacces

--- laravel/resources/views/contacte.blade.php
+++ laravel/resources/views/contacte.blade.php
@@ -6,10 +6,6 @@
 	<script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"
 		integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM="
 		crossorigin="">
-		var listener = new window.keypress.Listener();
-		listener.simple_combo("shift s", function(){
-		console.log("You pressed shift and s");
-		});
 	</script>
 
 </head>
@@ -35,6 +31,7 @@
 	<h1 class="body-h1">Vols visitar-nos?</h1>
 	<a href="#" onClick="getLocation()"><h3 class="body-h3">Ubica'ns al mapa</h3></a>
 	<div id="map"></div>
+
 	<script>
 		var map = L.map('map').setView([41.23114477320315, 1.7281181849031044], 18);
 		L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -42,6 +39,8 @@
 		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 		}).addTo(map);
 		var marker = L.marker([41.23114477320315, 1.7281181849031044]).addTo(map);
+		
+
 
 		function getLocation(e) {
 			if (navigator.geolocation) {
@@ -58,6 +57,12 @@
 			var circle = L.circle([position.coords.latitude, position.coords.longitude],{color:'blue', fillColor: '#0000FF', fillOpacity: 0.5, radius: 30}).addTo(map);
 		}
 		navigator.getLocation.getCurrentPosition(showPosition);
+
+		var listener = new window.keypress.Listener();
+		listener.simple_combo("ctrl alt g", function(e) {
+		e.alert("ESTO");
+    	console.log("You pressed shift and s");
+		});
 	</script>
 </body>
 <footer>
