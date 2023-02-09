97d40e15b17f768848c529c08a17ee8ed04e3024
xagape1 <xagape@fp.insjoaquimmir.cat>
2023-02-09 19:11:14 +0100

    JOEL Genio del script

--- laravel/resources/js/contacte/keypress.js
+++ laravel/resources/js/contacte/keypress.js
@@ -1,7 +1,23 @@
-		
+function showPosition(position) {
+    alert("Latitude: " + position.coords.latitude + 
+        "\nLongitude: " + position.coords.longitude)
+}
 //Dreceres de teclat
 var listener = new window.keypress.Listener();
-listener.simple_combo("ctrl alt g", function() {
-    alert("tus cordenadas son ")
-    console.log("You pressed shift and s");
+listener.simple_combo("ctrl alt g", function getLocation() {
+    if (navigator.geolocation) {
+
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+
+        x.innerHTML = "Geolocation is not supported by this browser.";
+
+    }
+
 });
+var listener2 = new window.keypress.Listener();
+listener2.simple_combo("ctrl alt c", function () {
+
+    L.map('map').setView([41.23114477320315, 1.7281181849031044], 18);
+
+})
